d3daa0d01edea36ab2d50c8eb876970123a3637c
Eduardo <eduardomr_@hotmail.com>
2024-04-06 20:55:38 +0200

    Ejercicios Realizados

---
+++ src/app/counter/counter.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  template: '<h1>Hola Counter</h1>'
+})
+
+export class CounterComponent {
+  constructor() { }
+
+
+}
